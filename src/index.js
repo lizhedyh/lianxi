94bc3ff56a80deacbb14e1f86be947636b045d0c
lizhedyh <1102197408@qq.com>
2019-08-05 15:16:02 +0800

    载入css和图片

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
 
 function component() {
   var  element = document.createElement('div');
   element.innerHTML = _.join(['Hello','webpack'],' ');
+  element.classList.add('hello');
+  
+
+  //将图片添加到我们现有的div
+  var myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
   return element;
 }
 document.body.appendChild(component());
