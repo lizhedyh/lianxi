9116735700bff15df84607cd0ed1b13b5130e93c
李哲 <lizhe21@maoyan.com>
2019-12-06 15:06:28 +0800

    cos获取url

--- src/ceshi/demo-cos.js
+++ src/ceshi/demo-cos.js
@@ -5,12 +5,10 @@ var cos = new COS({
     SecretId: 'AKIDQfjcw9J2kHguzebOUSFqJk6HKBtRt2mR',
     SecretKey: 'gCWhhV487vYzHOJ6UnwtfIku5rRuIE2e',
 });
-// 分片上传
-cos.sliceUploadFile({
-    Bucket: 'lizhe-1300719940', // Bucket 格式：test-1250000000
+var url = cos.getObjectUrl({
+    Bucket: 'lizhe-1300719940',
     Region: 'ap-beijing',
     Key: 'index.js',
-    FilePath: './index.js'
-}, function (err, data) {
-    console.log(err, data);
+    Sign: false
 });
+console.log(url);
